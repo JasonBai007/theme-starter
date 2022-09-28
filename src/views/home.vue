7a8ffe2876a6ed6e62612368d24d74ad31abcc7c
JasonBai007 <byygkcg@163.com>
2022-09-28 15:40:28 +0800

    完善页面

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="home">
-    <el-card class="box-card">
-      <div slot="header" class="header-wrap">
-        <!-- 切换白条和黑夜模式 -->
-        <themeSwitcher />
-        <!-- 使用主题切换组件 -->
-        <themePicker />
-      </div>
-      <div class="wrap">
-        <el-button size="small">默认按钮</el-button>
-        <el-button type="primary" size="small">主要按钮</el-button>
-        <el-button type="success" size="small">成功按钮</el-button>
-        <el-button type="warning" size="small">警告按钮</el-button>
-        <el-button type="danger" size="small">危险按钮</el-button>
-      </div>
-    </el-card>
+    <div class="inner">
+      <img src="../assets/logo.png" width="100px" />
+      <p>Vue-Element-Theme-Changer</p>
+      <el-card class="box-card">
+        <div slot="header" class="header-wrap">
+          <!-- 切换白条和黑夜模式 -->
+          <themeSwitcher />
+          <!-- 使用主题切换组件 -->
+          <themePicker />
+        </div>
+        <div class="wrap">
+          <el-button size="small">默认按钮</el-button>
+          <el-button type="primary" size="small">主要按钮</el-button>
+          <el-button type="success" size="small">成功按钮</el-button>
+          <el-button type="warning" size="small">警告按钮</el-button>
+          <el-button type="danger" size="small">危险按钮</el-button>
+        </div>
+      </el-card>
+    </div>
   </div>
 </template>
   
@@ -36,10 +40,19 @@ export default {
   width: 100%;
   height: 100%;
   display: flex;
-  .el-card {
-    width: 32vw;
+  .inner {
     margin: auto;
-    min-width: 400px;
+    margin-top: 130px;
+    text-align: center;
+  }
+  .el-card {
+    width: 35vw;
+    min-width: 350px;
+    margin-top: 20px;
+    .el-button {
+      margin-top: 5px;
+      margin-bottom: 5px;
+    }
   }
 }
 .home::v-deep .wrap {
